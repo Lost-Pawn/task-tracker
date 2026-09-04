afa55c4000ca82af66b86ebae3a1bac34311efba
Shashank Kumar Gupta <crushcastic@gmail.com>
2026-09-04 19:45:18 +0530

    feat: implement LoadTasks function to load tasks from JSON file

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,35 @@
 package storage
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"task-tracker/internal/task"
+)
+
+func LoadTasks() ([]task.Task, error) {
+	taskpath := filepath.Join(".", "tasks.json")
+	_, err := os.Stat(taskpath)
+	if errors.Is(err, fs.ErrNotExist) {
+		return []task.Task{}, nil
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to stat tasks file: %w", err)
+	}
+	tasks, err := os.Open(taskpath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open tasks file: %w", err)
+	}
+	defer tasks.Close()
+
+	var tasksList []task.Task
+	err = json.NewDecoder(tasks).Decode(&tasksList)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode tasks file: %w", err)
+	}
+
+	return tasksList, nil
+}
