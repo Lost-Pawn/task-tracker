53006e2b953b2e8eabe3e86245ae38d4a0d750c0
Shashank Kumar Gupta <crushcastic@gmail.com>
2026-09-04 18:29:28 +0530

    refactor: updated fields in task struct

--- internal/task/task.go
+++ internal/task/task.go
@@ -6,6 +6,6 @@ type Task struct {
 	ID 			int 		`json:"id"`;
 	Description string 		`json:"description"`;
 	Status 		string 		`json:"status"`;
-	CreatedAt 	time.Time 	`json:"created_at"`;
-	UpdatedAt 	time.Time 	`json:"updated_at"`;
+	CreatedAt 	time.Time 	`json:"createdAt"`;
+	UpdatedAt 	time.Time 	`json:"updatedAt"`;
 }
