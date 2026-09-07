cbad45877a42cbe7f69a523afa5ef943f6b57816
Shashank Kumar Gupta <crushcastic@gmail.com>
2026-09-07 22:20:59 +0530

    feat: implement AddTask function to add new tasks and save to storage

--- internal/task/task.go
+++ internal/task/task.go
@@ -1,6 +1,10 @@
 package task
 
-import "time"
+import (
+	"fmt"
+	"task-tracker/internal/storage"
+	"time"
+)
 
 type Task struct {
 	ID 			int 		`json:"id"`;
@@ -8,4 +12,35 @@ type Task struct {
 	Status 		string 		`json:"status"`;
 	CreatedAt 	time.Time 	`json:"createdAt"`;
 	UpdatedAt 	time.Time 	`json:"updatedAt"`;
+}
+
+func AddTask(tasks []Task, description string) ([]Task, error) {
+	newID := 1
+	
+	for _, task := range tasks {
+		if task.ID >= newID {
+			newID = task.ID + 1
+		}
+	}
+
+	if description == "" {
+		return tasks, fmt.Errorf("task description cannot be empty")
+
+	}
+
+	newTask := Task{
+		ID:          newID,
+		Description: description,
+		Status:      "todo",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	tasks = append(tasks, newTask)
+	storageErr := storage.SaveTasks(tasks)
+	if storageErr != nil {
+		return tasks, fmt.Errorf("failed to save tasks: %w", storageErr)
+	}
+
+	return tasks, nil
 }
